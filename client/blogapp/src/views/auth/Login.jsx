f289260e503eb55cd25c7323a61b5884d3abf7e5
thushara <thushararamadas@gmail.com>
2022-10-20 14:59:36 +0530

    register and login page settt

--- client/blogapp/src/views/auth/Login.jsx
+++ client/blogapp/src/views/auth/Login.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+    TextField,
+    Stack,
+    Typography,
+    Checkbox,
+    FormControlLabel,
+    Button,
+} from "@mui/material";
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div>
+            <>
+                {/* <h1>hai</h1> */}
+                <MainDiv>
+                    {/* <Box sx="50%"> */}
+                    <Stack spacing={3}>
+                        <Typography variant="h5" align="center">
+                            Sign In
+                        </Typography>
+                        <TextField
+                            id="outlined-basic"
+                            label="User Name"
+                            variant="outlined"
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Password"
+                            variant="outlined"
+                        />
+
+                        <Button variant="contained">SIGN IN</Button>
+                    </Stack>
+                    {/* </Box> */}
+                </MainDiv>
+            </>
+            ;
+        </div>
+    );
 };
 
 export default Login;
+const MainDiv = styled.div`
+    width: 30%;
+    padding: 50px;
+    margin: 0 auto;
+    /* border: 1px solid #000; */
+`;
--- client/blogapp/src/views/auth/Register.jsx
+++ client/blogapp/src/views/auth/Register.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import {
+    TextField,
+    Stack,
+    Typography,
+    Checkbox,
+    FormControlLabel,
+    Button,
+} from "@mui/material";
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return <div>Register</div>;
+    return (
+        <>
+            {/* <h1>hai</h1> */}
+            <MainDiv>
+                {/* <Box
+                    component="img"
+                    sx={{
+                        height: 233,
+                        width: 350,
+                    }}
+                    alt="The house from the offer."
+                    src="FaLock"
+                /> */}
+                {/* <Box sx="50%"> */}
+                <Stack spacing={3}>
+                    <Typography variant="h5" align="center">
+                        Sign Up
+                    </Typography>
+                    <TextField
+                        id="outlined-basic"
+                        label="User Name"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Email Address"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox defaultChecked />}
+                        label="content"
+                    />
+                    <Button variant="contained">SIGN UP</Button>
+                    <Link href="#" color="primary" align="right">
+                        Already have an account? Sign in
+                    </Link>
+                </Stack>
+                {/* </Box> */}
+            </MainDiv>
+        </>
+    );
 };
 
 export default Register;
+const MainDiv = styled.div`
+    width: 30%;
+    padding: 50px;
+    margin: 0 auto;
+    /* border: 1px solid #000; */
+`;
