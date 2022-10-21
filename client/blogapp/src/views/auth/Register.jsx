d6c301cbfec849da752bb17f339c23d6bbf16702
nikhil <nikicvr@gamail.com>
2022-10-21 09:01:33 +0530

    verified

--- client/blogapp/src/views/auth/Register.jsx
+++ client/blogapp/src/views/auth/Register.jsx
@@ -14,18 +14,9 @@ import { Link } from "react-router-dom";
 const Register = () => {
     return (
         <>
-            {/* <h1>hai</h1> */}
+            
             <MainDiv>
-                {/* <Box
-                    component="img"
-                    sx={{
-                        height: 233,
-                        width: 350,
-                    }}
-                    alt="The house from the offer."
-                    src="FaLock"
-                /> */}
-                {/* <Box sx="50%"> */}
+                
                 <Stack spacing={3}>
                     <Typography variant="h5" align="center">
                         Sign Up
@@ -54,7 +45,7 @@ const Register = () => {
                         Already have an account? Sign in
                     </Link>
                 </Stack>
-                {/* </Box> */}
+                
             </MainDiv>
         </>
     );
@@ -65,5 +56,5 @@ const MainDiv = styled.div`
     width: 30%;
     padding: 50px;
     margin: 0 auto;
-    /* border: 1px solid #000; */
+    
 `;
